a10d6bf38a308d8d763be9092b463e5cd6b00026
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-05 02:54:20 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(input_string = "Squawk!")
-  put input_string
+  puts input_string
   return input_string
 end
